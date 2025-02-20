46ccd00e7fd576153e7653b42e975a63a7b8986b
Juan Zurita <juanzq10dev@gmail.com>
2025-02-19 21:32:15 -0400

    RENAME: Rename 03 and 04 capstones

--- capstones/04-color-list-provider/src/App.tsx
+++ capstones/04-color-list-provider/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import ColorList from './components/ColorList'
+import AddColorForm from './components/Form'
 
+// Color provider used on main.tsx
 function App() {
   return (
     <>
+      <AddColorForm></AddColorForm>
       <ColorList></ColorList>
     </>
   )
